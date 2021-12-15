dc4360118711026143632ef493e9daeaeadba2f1
Gianmarco Barca <Gianmarco.technology@outlook.com>
2021-12-14 19:07:03 -0500

    Phase-5 complete.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const util = require('util');
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const signTokenAsync = util.promisify(jwt.sign);
 
@@ -20,17 +21,41 @@ async function signup(req, res, next) {
         email: user.email,
       },
       process.env.JWT_SECRET,
-      {expiresIn: process.env.JWT_EXPIRES_IN}
+      { expiresIn: process.env.JWT_EXPIRES_IN }
     );
 
-    res.status(201).send({status: 'success', data: {token}});
+    res.status(201).send({ status: 'success', data: { token } });
   } catch (err) {
     next(err);
   }
 }
 
 async function login(req, res, next) {
-  res.send('Welcome to the login page!');
+  // 1) lookup user with email, exit if false
+  // 2) encrypt password and check if it matches password in db, exit if false
+  // 3) return token
+
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+      throw new AppError('Incorrect email or passowrd. Please try again.', 400);
+    }
+
+    const token = await signTokenAsync(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(200).send({ status: 'success', data: { token } });
+  } catch (err) {
+    next(err);
+  }
 }
 
-module.exports = {signup, login};
+module.exports = { signup, login };
