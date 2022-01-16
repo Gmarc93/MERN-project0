30cceb6ac35c00c03a8fce37a6f60fd32a6a7343
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-01-16 15:32:24 -0500

    Update sendEmail.js

--- api/utils/sendEmail.js
+++ api/utils/sendEmail.js
@@ -1,21 +1,27 @@
 const nodemailer = require('nodemailer');
 
 async function sendEmail(data) {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SEND_EMAIL_HOST,
-    port: process.env.SEND_EMAIL_PORT,
-    auth: {
-      user: process.env.SEND_EMAIL_USERNAME,
-      pass: process.env.SEND_EMAIL_PASSWORD,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-  await transporter.sendMail({
-    from: 'MERN-project0.io',
-    to: data.email,
-    subject: data.subject,
-    text: data.message,
-  });
+    const info = await transporter.sendMail({
+      from: 'MERN-project0',
+      to: data.email,
+      subject: data.subject,
+      text: data.message,
+    });
+
+    return info;
+  } catch (err) {
+    throw err;
+  }
 }
 
 module.exports = sendEmail;
