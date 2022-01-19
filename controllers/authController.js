56a57562ab25e0636c801648ae4ce808ecc14b5a
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-01-18 22:08:42 -0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const util = require('util');
 const crypto = require('crypto');
 const User = require('../models/userModel');
@@ -6,28 +8,28 @@ const bcrypt = require('bcrypt');
 const AppError = require('../api/utils/AppError');
 const sendEmail = require('../api/utils/sendEmail');
 
+// Promisify JWT functions
 const signTokenAsync = util.promisify(jwt.sign);
-const verifyTokenAsync = util.promisify(jwt.verify);
+const verifyTokenAsyc = util.promisify(jwt.verify);
 
 async function routeProtection(req, res, next) {
   try {
     const {authorization} = req.headers;
 
-    if (!authorization) {
-      throw new AppError('Error. Please log in to view page.', 400);
-    }
-
-    if (!authorization.startsWith('Bearer')) {
-      throw new AppError('Error. Please log in to view page.', 400);
+    if (
+      !authorization ||
+      !authorization.startsWith('Bearer') ||
+      !authorization.split(' ')[1]
+    ) {
+      throw new AppError('Please login to view page.', 400);
     }
 
     const token = authorization.split(' ')[1];
-    const decoded = await verifyTokenAsync(token, process.env.JWT_SECRET);
+    const decoded = await verifyTokenAsyc(token, process.env.JWT_SECRET);
+
     const user = await User.findById(decoded.id);
 
-    if (!user) {
-      throw new AppError('Error. User does not exist.', 400);
-    }
+    if (!user) throw new AppError('User no longer exists.', 400);
 
     req.user = user;
     next();
@@ -37,16 +39,18 @@ async function routeProtection(req, res, next) {
 }
 
 function restrictToAdmin(req, res, next) {
-  if (req.user.role !== 'admin')
+  if (req.user.role !== 'admin') {
     return next(
       new AppError('You do not have permission to view this page.', 400)
     );
+  }
   next();
 }
 
 async function signup(req, res, next) {
+  let user = undefined;
   try {
-    const user = await User.create({
+    user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
@@ -63,7 +67,51 @@ async function signup(req, res, next) {
       {expiresIn: process.env.JWT_EXPIRES_IN}
     );
 
-    res.status(201).send({status: 'success', data: {token}});
+    res
+      .status(201)
+      .cookie('jwt', token, {
+        expires: new Date(
+          Date.now() + 1000 * 60 * process.env.JWT_COOKIE_EXPIRES_IN
+        ),
+        httpOnly: true, // Cookie is only accessible by web server
+        // secure: true,
+      })
+      .send({status: 'success', data: {token}});
+  } catch (err) {
+    // Delete user if created and response is unsuccessful
+    if (user) await User.deleteOne({_id: user._id});
+    next(err);
+  }
+}
+
+async function login(req, res, next) {
+  try {
+    const user = await User.findOne({email: req.body.email});
+
+    if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+      throw new AppError('Incorrect email or password. Please try again', 400);
+    }
+
+    const token = await signTokenAsync(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {expiresIn: process.env.JWT_EXPIRES_IN}
+    );
+
+    res
+      .status(200)
+      .cookie('jwt', token, {
+        expires: new Date(
+          Date.now() + 1000 * 60 * process.env.JWT_COOKIE_EXPIRES_IN
+        ),
+        httpOnly: true, // Cookie is only accessible by web server
+        // secure: true,
+      })
+      .send({status: 'success', data: {token}});
   } catch (err) {
     next(err);
   }
@@ -72,25 +120,23 @@ async function signup(req, res, next) {
 async function forgotPassword(req, res, next) {
   let user = undefined;
   try {
-    // locate user with email
-    user = await User.findOne({ email: req.body.email });
-    if (!user) throw new AppError('User does not exist.', 404);
+    user = await User.findOne({email: req.body.email});
+    if (!user) throw new AppError('User does not exist.', 400);
 
-    // create and save password token
     const cryptoToken = crypto.randomBytes(32).toString('hex');
     const bcryptToken = await bcrypt.hash(cryptoToken, 12);
 
-    user.passwordResetToken = bcryptToken;
-    user.passwordResetExpires = Date.now() + 1000 * 60 * 15;
-    user.save({ validateBeforeSave: false });
+    user.passwordReset = bcryptToken;
+    user.passwordResetExpiresIn = Date.now() + 1000 * 60 * 10;
+    user._required = false;
+    await user.save();
 
-    // send link to user's email
-    const url = `localhost:8000/api/v1/users/passwordReset/${cryptoToken}/${user.id}`;
+    const url = `localhost:8000/api/v1/users/forgotPassword/${cryptoToken}/${user._id}`;
     const message = `Forgot password? Please click on the link below to reset your password: ${url}.`;
 
     const info = await sendEmail({
       email: user.email,
-      subject: 'Password Reset (expires in 10 min)',
+      subject: 'Password Reset Token (expires in 10 min.)',
       message,
     });
 
@@ -100,9 +146,10 @@ async function forgotPassword(req, res, next) {
     });
   } catch (err) {
     if (user) {
-      user.passwordResetToken = undefined;
-      user.passwordResetExpires = undefined;
-      user.save({ validateBeforeSave: false });
+      user.passwordReset = undefined;
+      user.passwordResetExpiresIn = undefined;
+      user._required = false;
+      await user.save();
     }
     next(err);
   }
