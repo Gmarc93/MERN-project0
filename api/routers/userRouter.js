f08dc2ca7b19ed9c65d6fb8a30d4c65d24ac7760
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-01-18 22:10:36 -0500

    Update userRouter.js

--- api/routers/userRouter.js
+++ api/routers/userRouter.js
@@ -3,24 +3,22 @@ const authController = require('../../controllers/authController');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) =>
-  res.send('Welcome to the users home page!')
-);
+router.get('/', (req, res) => res.send('Welcome to the users home page!'));
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 
-router.get('/private', authController.routeProtection, (req, res, next) => {
-  res.send('Welcome to the users restricted route!');
+router.get('/private', authController.routeProtection, (req, res) => {
+  res.send('Welcome to the private page!');
 });
 
 router.get(
   '/privateAdmin',
   authController.routeProtection,
   authController.restrictToAdmin,
-  (req, res, next) => {
-    res.send('Welcome to the admins restricted route!');
+  (req, res) => {
+    res.send('Welcome to the privateAdmin page!');
   }
 );
 
