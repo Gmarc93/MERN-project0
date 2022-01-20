1f12471c13172b215c56b278c904d15379bee85b
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-01-20 17:52:39 -0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,38 @@
-const AppError = require('../api/utils/AppError');
-const User = require('../models/userModel');
-const filterObj = require('../utils/filterObj');
+const util = require('util');
+const jwt = require('jsonwebtoken');
+
+const signTokenAsync = util.promisify(jwt.sign);
 
 async function updateUser(req, res, next) {
   try {
-    const filteredBody = filterObj(req.body, ['name', 'email']);
+    const user = req.user;
 
-    await User.findByIdAndUpdate(req.user.id, filteredBody, {
-      new: true,
-      runValidators: true,
-    });
+    // Can also define filterObj
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user._required = true; // incase name is empty string
+    await user.save({validateModifiedOnly: true});
+
+    const token = await signTokenAsync(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {expiresIn: process.env.JWT_EXPIRES_IN}
+    );
 
     res
       .status(200)
-      .send({status: 'success', message: 'User update completed.'});
-
-    // You may need to update JWT token and send it back with the
-    // updated information
+      .cookie('jwt', token, {
+        expires: new Date(
+          Date.now() + 1000 * 60 * process.env.JWT_COOKIE_EXPIRES_IN
+        ),
+        httpOnly: true, // Cookie is only accessible by web server
+        // secure: true,
+      })
+      .send({status: 'success', data: {token}});
   } catch (err) {
     next(err);
   }
