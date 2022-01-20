792e05b32d2d76c4457576ad95411c3f79019a0d
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-01-20 18:18:42 -0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,41 @@
-const AppError = require('../api/utils/AppError');
+const util = require('util');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
-const filterObj = require('../utils/filterObj');
+const AppError = require('../api/utils/AppError');
+
+const signTokenAsync = util.promisify(jwt.sign);
 
 async function updateUser(req, res, next) {
   try {
-    const filteredBody = filterObj(req.body, ['name', 'email']);
+    const user = req.user;
 
-    await User.findByIdAndUpdate(req.user.id, filteredBody, {
-      new: true,
-      runValidators: true,
-    });
+    // Can also define filterObj
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user._required = true; // incase name is empty string
+    await user.save({validateModifiedOnly: true});
+
+    const token = await signTokenAsync(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {expiresIn: process.env.JWT_EXPIRES_IN}
+    );
 
     res
       .status(200)
-      .send({status: 'success', message: 'User update completed.'});
-
-    // You may need to update JWT token and send it back with the
-    // updated information
+      .cookie('jwt', token, {
+        expires: new Date(
+          Date.now() + 1000 * 60 * process.env.JWT_COOKIE_EXPIRES_IN
+        ),
+        httpOnly: true, // Cookie is only accessible by web server
+        // secure: true,
+      })
+      .send({status: 'success', data: {token}});
   } catch (err) {
     next(err);
   }
@@ -24,9 +43,18 @@ async function updateUser(req, res, next) {
 
 async function deleteUser(req, res, next) {
   try {
-    await User.deleteOne({id: req.id});
+    const user = req.user;
+    if (
+      !req.body.password ||
+      !(await bcrypt.compare(req.body.password, user.password))
+    ) {
+      throw new AppError('Incorrect password. Please try again.', 400);
+    }
 
-    res.status(200).send({status: 'success', message: 'User deleted.'});
+    await User.deleteOne({email: user.email});
+
+    // Render logged out page
+    res.status(200).send({status: 'success', message: 'User deleted'});
   } catch (err) {
     next(err);
   }
