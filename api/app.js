d351325fa7831bdda61c78d0b09d9a0fa68db631
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-01-19 21:19:14 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -5,14 +5,15 @@ const userRouter = require('./routers/userRouter');
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+// Routes
 app.get('/', (req, res, next) => res.send('Welcome to the home page!'));
-
 app.use('/api/v1/users', userRouter);
-
 app.all('*', (req, res, next) => next(new AppError('Page not found.', 404)));
 
+// Error handler
 app.use(globalErrorHandler);
 
 module.exports = app;
