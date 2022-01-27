40214e23eb3f7648318c175e2b5cbc3b1861209b
Gmarc93 <Gianmarco.technology@outlook.com>
2022-01-26 19:23:31 -0500

    Phase-18 complete.

--- controllers/productController.js
+++ controllers/productController.js
@@ -67,4 +67,51 @@ async function getAllProducts(req, res, next) {
   }
 }
 
-module.exports = {createProduct, getProduct, getAllProducts};
+async function updateProduct(req, res, next) {
+  try {
+    const product = await Product.findById(req.params.id).select(
+      '_id name description price imageCover'
+    );
+
+    if (!product) throw new AppError('Product does not exist.', 404);
+
+    product.name = req.body.name || product.name;
+    product.description = req.body.description || product.description;
+    product.price = req.body.price || product.price;
+    product.imageCover = req.body.imageCover || product.imageCover;
+    product._require = true;
+    product.save();
+
+    res.status(200).send({
+      satus: 'success',
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteProduct(req, res, next) {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) throw new AppError('Product does not exist.', 404);
+
+    res.status(200).send({
+      satus: 'success',
+      message: 'Product deleted.',
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = {
+  createProduct,
+  getProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+};
