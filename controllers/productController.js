a4de5740de519699ccf0626511b9c7e5b3a9f9ef
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-01-31 21:27:34 -0500

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,13 +14,7 @@ async function createProduct(req, res, next) {
     res.status(201).send({
       satus: 'success',
       data: {
-        product: {
-          id: product._id,
-          name: product.name,
-          description: product.description,
-          price: product.price,
-          imageCover: product.imageCover,
-        },
+        product,
       },
     });
   } catch (err) {
@@ -30,15 +24,10 @@ async function createProduct(req, res, next) {
 
 async function getProduct(req, res, next) {
   try {
-    const product = await Product.findById(req.params.id)
-      .select(
-        '-_required'
-        // The code above may be put into a pre hook
-      )
-      .populate('reviews', '-_required');
+    const product = await Product.findById(req.params.id);
 
-    // Need to implement limits or pagination for total reviews
     if (!product) throw new AppError('Product does not exist.', 404);
+
     // Make sure to add id cast error into globalErrorHandler in the future
 
     res.status(201).send({
@@ -54,9 +43,7 @@ async function getProduct(req, res, next) {
 
 async function getAllProducts(req, res, next) {
   try {
-    const products = await Product.find().select(
-      '_id name description price imageCover'
-    );
+    const products = await Product.find();
 
     res.status(201).send({
       satus: 'success',
@@ -69,51 +56,4 @@ async function getAllProducts(req, res, next) {
   }
 }
 
-async function updateProduct(req, res, next) {
-  try {
-    const product = await Product.findById(req.params.id).select(
-      '_id name description price imageCover'
-    );
-
-    if (!product) throw new AppError('Product does not exist.', 404);
-
-    product.name = req.body.name || product.name;
-    product.description = req.body.description || product.description;
-    product.price = req.body.price || product.price;
-    product.imageCover = req.body.imageCover || product.imageCover;
-    product._require = true;
-    product.save();
-
-    res.status(200).send({
-      satus: 'success',
-      data: {
-        product,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-}
-
-async function deleteProduct(req, res, next) {
-  try {
-    const product = await Product.findByIdAndDelete(req.params.id);
-
-    if (!product) throw new AppError('Product does not exist.', 404);
-
-    res.status(200).send({
-      satus: 'success',
-      message: 'Product deleted.',
-    });
-  } catch (err) {
-    next(err);
-  }
-}
-
-module.exports = {
-  createProduct,
-  getProduct,
-  getAllProducts,
-  updateProduct,
-  deleteProduct,
-};
+module.exports = {createProduct, getProduct, getAllProducts};
