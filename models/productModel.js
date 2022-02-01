3965057c0d1530ef30ed5cc5d0c4ceb7aae80a17
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-01-31 20:16:53 -0500

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -7,43 +7,63 @@ function requiredValidator() {
   return this._required === true;
 }
 
+function remove_id__v(doc, res, options) {
+  delete res._id;
+  delete res.__v;
+}
+
 // Schema
-const productSchema = mongoose.Schema({
-  _required: {
-    type: Boolean,
-    default: true,
+const productSchema = mongoose.Schema(
+  {
+    _required: {
+      type: Boolean,
+      default: true,
+    },
+    name: {
+      type: String,
+      required: requiredValidator,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      maxlength: 40,
+    },
+    description: {
+      type: String,
+      required: requiredValidator,
+      trim: true,
+      lowercase: true,
+    },
+    price: {
+      type: Number,
+      required: requiredValidator,
+    },
+    ratingsAverage: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    imageCover: {
+      type: String,
+      default: 'productImageDefault.jpg',
+    },
   },
-  name: {
-    type: String,
-    required: requiredValidator,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    maxlength: 40,
-  },
-  description: {
-    type: String,
-    required: requiredValidator,
-    trim: true,
-    lowercase: true,
-  },
-  price: {
-    type: Number,
-    required: requiredValidator,
-  },
-  ratingsAverage: {
-    type: Number,
-    min: 1,
-    max: 5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  imageCover: {
-    type: String,
-    default: 'productImageDefault.jpg',
-  },
+  {
+    // Virtuals will be visible on res.send()
+    toJSON: {virtuals: true, transform: remove_id__v},
+
+    // Virtuals will be virible on console.log()
+    toObject: {virtuals: true, transform: remove_id__v},
+  }
+);
+
+productSchema.virtual('reviews', {
+  ref: 'Review',
+  localField: '_id',
+  foreignField: 'product',
 });
 
 const Product = mongoose.model('Product', productSchema);
