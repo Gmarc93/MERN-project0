1b64507ee0aa637a803045f38d071043dda38a84
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-02-05 12:40:12 -0500

    Update reviewRouter.js

--- api/routers/reviewRouter.js
+++ api/routers/reviewRouter.js
@@ -4,12 +4,14 @@ const reviewController = require('../../controllers/reviewController');
 
 const router = express.Router();
 
-router.route('/').get(reviewController.getAllReviews);
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(authController.routeProtection, reviewController.createReview);
 
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .post(authController.routeProtection, reviewController.createReview)
   .patch(authController.routeProtection, reviewController.updateReview)
   .delete(authController.routeProtection, reviewController.deleteReview);
 
