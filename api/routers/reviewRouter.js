b8737685e8799e3bbdfb80788926ecb194383ec5
Gianmarco Rengifo <64345463+Gmarc93@users.noreply.github.com>
2022-02-05 23:42:17 -0500

    Update reviewRouter.js

--- api/routers/reviewRouter.js
+++ api/routers/reviewRouter.js
@@ -2,15 +2,18 @@ const express = require('express');
 const authController = require('../../controllers/authController');
 const reviewController = require('../../controllers/reviewController');
 
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
-  .post(authController.routeProtection, reviewController.createReview);
+  .post(authController.routeProtection, reviewController.createReview)
+
+  // The middleware below must only be used in development!
+  .delete(reviewController.deleteAllReviews);
 
 router
-  .route('/:id')
+  .route('/:reviewId')
   .get(reviewController.getReview)
   .patch(authController.routeProtection, reviewController.updateReview)
   .delete(authController.routeProtection, reviewController.deleteReview);
